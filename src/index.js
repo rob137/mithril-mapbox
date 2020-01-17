ee03998be4d8f5451a28afbea65490d868256d2f
rob <robertaxelkirby@gmail.com>
2020-01-17 16:34:50 +0000

    Add mapbox

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 const m = require('mithril');
+const mapboxgl = require('mapbox-gl');
 const root = document.body;
 
 const Map = {
-  view: () => {
-    return m('p', 'test');
-  },
+  view: () => m('div', {id: 'map'}),
 };
-console.log(132);
 
 m.mount(root, Map);
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoicm9iZXJ0YXhlbGtpcmJ5IiwiYSI6ImNqOTE3aHJxajJjbHkyeHBlaTR6M2FqOWoifQ.zC4G-gKN2J6kUWh6aIRPpw';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  zoom: 13,
+  center: [-120.385471, 46.380045],
+  style: 'mapbox://styles/mapbox/satellite-v9',
+  doubleClickZoom: false
+});
+
+map.on('load', () => map.resize());
